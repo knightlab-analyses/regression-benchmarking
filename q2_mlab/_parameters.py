0044a7510f81699ed483c7fb68c4f1aa20c8f602
Imran McGrath <i.mcgrath207@gmail.com>
2020-07-23 08:47:54 -0700

    parameter grids

--- q2_mlab/_parameters.py
+++ q2_mlab/_parameters.py
@@ -49,7 +49,7 @@ class ParameterGrids:
             "RandomForestClassifier": {
                 "n_estimators": [1000, 5000],
                 "criterion": ["gini", "entropy"],
-                "max_features": ["auto", "sqrt", "log2"],
+                "max_features": ["sqrt", "log2", None] + list(np.arange(0.01, 1, 0.2)),
                 "max_depth": [None],
                 "n_jobs": [-1],
                 "random_state": [2018],
