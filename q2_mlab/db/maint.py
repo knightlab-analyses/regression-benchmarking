e0b602ebf20e6c86a9ce09839c7140d6de7058e0
gwarmstrong <garmstrong@colgate.edu>
2020-09-17 10:58:43 -0700

    NEW sanity check module that makes checks on database post-hoc

--- q2_mlab/db/maint.py
+++ q2_mlab/db/maint.py
@@ -1,8 +1,7 @@
-import json
 import pandas as pd
 from sqlalchemy.engine import Engine
 from sqlalchemy import create_engine as sql_create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker as sql_sessionmaker
 from datetime import datetime
 from qiime2 import Artifact
 from q2_mlab.db.schema import RegressionScore, Parameters, Base
@@ -34,11 +33,28 @@ def create(db_file: Optional[str] = None, echo=True) -> Engine:
     return engine
 
 
+def sessionmaker(db_file=None, engine=None, echo=False):
+    if db_file is not None and engine is None:
+        engine = create_engine(db_file, echo=echo)
+    elif engine is not None and db_file is None:
+        pass
+    elif engine is not None and db_file is not None:
+        raise ValueError(f"Parameters 'db_file' and 'engine' are mutually "
+                         f"exclusive. Got: db_file={db_file}, engine={engine}")
+    else:
+        raise ValueError(f"Either 'db_file' or 'engine' is required for "
+                         f"sessionmaker.")
+
+    # otherwise assume it is a proper engine
+    Session = sql_sessionmaker(bind=engine)
+    return Session
+
+
 def add(engine: Engine, results: pd.DataFrame, parameters: dict,
         dataset: str, target: str, level: str, algorithm: str,
         artifact_uuid: str,
         ) -> None:
-    Session = sessionmaker(bind=engine)
+    Session = sessionmaker(engine=engine)
     session = Session()
 
     # check if parameters exists in db
